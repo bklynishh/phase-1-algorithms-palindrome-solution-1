eb5a611dc1a1b238e23e8b4f6da55f854f98b178
Hannah Maratea <maratea101@gmail.com>
2022-12-03 20:34:23 -0500

    commit tree a6f862a091abb7a108e867c10581e22c8c0feed3

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+const wordArray = word.split("");
+const reversedWordArray = wordArray.reverse();
+const reversedWord = reversedWordArray.join("");
+return reversedWord;
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
 
-/*
-  Add written explanation of your solution here
-*/
+return word === reversedWord;
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
